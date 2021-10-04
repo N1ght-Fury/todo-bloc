15205a0c237496adca9d5072696b6b5f90045890
Talha <talha.kerpicci@gmail.com>
2021-10-04 18:24:41 +0300

    Small fixes

--- lib/logic/cubit/user_cubit.dart
+++ lib/logic/cubit/user_cubit.dart
@@ -1,16 +1,17 @@
 import 'dart:convert';
-
+import 'dart:math';
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
-import 'package:todo_bloc/data/services/api.dart';
-import 'package:todo_bloc/logic/cubit/todo_cubit.dart';
 
-import '../../data/model/models.dart';
+import '../../data/services/api.dart';
+import '../../data/model/user_models.dart';
+
+import 'todo_cubit.dart';
 
 part 'user_state.dart';
 
-class UserCubit extends Cubit<UserState> /* with HydratedMixin */ {
+class UserCubit extends Cubit<UserState> with HydratedMixin {
   final Api api;
 
   UserCubit({required this.api}) : super(UserInitial());
@@ -27,16 +28,23 @@ class UserCubit extends Cubit<UserState> /* with HydratedMixin */ {
     }
   }
 
+  Future<void> logout() async {
+    emit(UserInitial());
+  }
+
   @override
   UserState fromJson(Map<String, dynamic> json) {
+    print('inside fromJson');
     return UserLoggedIn.fromMap(json);
   }
 
   @override
-  Map<String, dynamic> toJson(UserState state) {
-    if (state.user != null) {
-      return state.user!.toMap();
+  Map<String, dynamic>? toJson(UserState state) {
+    print('inside toJson');
+    print(state is UserLoggedIn);
+    if (state is UserLoggedIn) {
+      state.toJson();
     }
-    return {};
+    return null;
   }
 }
