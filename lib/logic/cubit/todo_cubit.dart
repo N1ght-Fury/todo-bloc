2b7694697a3b99fd58275907cbfe3433aecfda54
Talha <talha.kerpicci@gmail.com>
2021-10-04 18:24:05 +0300

    Added userstate as parameter

--- lib/logic/cubit/todo_cubit.dart
+++ lib/logic/cubit/todo_cubit.dart
@@ -1,10 +1,12 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:todo_bloc/data/model/models.dart';
-import 'package:todo_bloc/logic/cubit/user_cubit.dart';
+import 'package:flutter/material.dart';
+import '../../data/model/todo_models.dart';
+import 'user_cubit.dart';
 
 part 'todo_state.dart';
 
 class TodoCubit extends Cubit<TodoState> {
-  TodoCubit() : super(TodoInitial());
+  final UserState userState;
+  TodoCubit({required this.userState}) : super(TodoInitial());
 }
