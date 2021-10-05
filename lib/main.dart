d9dc885ca080f07761379df330024e64c38dd408
Talha <talha.kerpicci@gmail.com>
2021-10-05 17:46:13 +0300

    Added logger message

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:logger/logger.dart';
 import 'package:path_provider/path_provider.dart';
-import 'logic/cubit/user_cubit.dart';
 
 import 'data/services/api.dart';
+import 'logic/cubit/user_cubit.dart';
 import 'logic/utility/app_bloc_observer.dart';
 import 'presentation/router/app_router.dart';
 
@@ -40,6 +41,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Logger().log(Level.info, 'Inside Main');
     return MultiBlocProvider(
       providers: [
         BlocProvider<UserCubit>(
@@ -47,19 +49,18 @@ class MyApp extends StatelessWidget {
           /* lazy: false, */
         ),
       ],
-      child: Builder(
-        builder: (context) {
-          return MaterialApp(
-            title: 'Todo App',
-            theme: ThemeData(
-              primarySwatch: Colors.blue,
-              visualDensity: VisualDensity.adaptivePlatformDensity,
-            ),
-            initialRoute: context.read<UserCubit>().state is UserLoggedIn ? '/todos' : '/login',
-            onGenerateRoute: appRouter.onGenerateRoute,
-          );
-        }
-      ),
+      child: Builder(builder: (context) {
+        Logger().log(Level.info, 'Is user logged in: ' + ((context.read<UserCubit>().state is UserLoggedIn) ? 'yes' : 'no'));
+        return MaterialApp(
+          title: 'Todo App',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+          ),
+          initialRoute: context.read<UserCubit>().state is UserLoggedIn ? '/todos' : '/login',
+          onGenerateRoute: appRouter.onGenerateRoute,
+        );
+      }),
     );
   }
 }
