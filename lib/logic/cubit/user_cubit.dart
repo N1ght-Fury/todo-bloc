7d04caaeecf8b542813f9ab390f4e405a38f3c80
Talha <talha.kerpicci@gmail.com>
2021-10-05 17:46:58 +0300

    Added loggers and fixed user state not changing bug

--- lib/logic/cubit/user_cubit.dart
+++ lib/logic/cubit/user_cubit.dart
@@ -1,20 +1,21 @@
 import 'dart:convert';
-import 'dart:math';
+
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:logger/logger.dart';
 
-import '../../data/services/api.dart';
 import '../../data/model/user_models.dart';
-
-import 'todo_cubit.dart';
+import '../../data/services/api.dart';
 
 part 'user_state.dart';
 
 class UserCubit extends Cubit<UserState> with HydratedMixin {
   final Api api;
 
-  UserCubit({required this.api}) : super(UserInitial());
+  UserCubit({required this.api}) : super(UserInitial()) {
+    Logger().log(Level.warning, 'CURRENT USER STATE IS $state');
+  }
 
   Future<void> login(int id) async {
     emit(UserProcessing());
@@ -29,22 +30,33 @@ class UserCubit extends Cubit<UserState> with HydratedMixin {
   }
 
   Future<void> logout() async {
+    await HydratedBloc.storage.clear();
     emit(UserInitial());
   }
 
   @override
   UserState fromJson(Map<String, dynamic> json) {
-    print('inside fromJson');
+    Logger().log(Level.info, 'Inside User Cubit -> fromJson function');
+    Logger().log(Level.info, 'Json is: $json');
+
+    // Code below causes infinite loop?
+    /* Logger().log(Level.wtf, 'CURRENT USER STATE: $state'); */
+
+    if (json.isEmpty) {
+      return UserInitial();
+    }
+
     return UserLoggedIn.fromMap(json);
   }
 
   @override
   Map<String, dynamic>? toJson(UserState state) {
-    print('inside toJson');
-    print(state is UserLoggedIn);
+    Logger().log(Level.info, 'Inside User Cubit -> toJson function');
     if (state is UserLoggedIn) {
-      state.toJson();
+      Logger().log(Level.info, 'Current user info is: ${state.toMap().toString()}');
+      return state.toMap();
     }
-    return null;
+    Logger().log(Level.info, 'Setting user info to null!');
+    return UserInitial().toMap();
   }
 }
