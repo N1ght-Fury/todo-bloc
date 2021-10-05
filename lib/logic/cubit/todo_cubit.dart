e9ae978131cee50a71a769d3ec7c7ecc6996c7eb
Talha <talha.kerpicci@gmail.com>
2021-10-05 17:46:37 +0300

    Added logger

--- lib/logic/cubit/todo_cubit.dart
+++ lib/logic/cubit/todo_cubit.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
 import '../../data/model/todo_models.dart';
 import 'user_cubit.dart';
 
@@ -8,5 +9,7 @@ part 'todo_state.dart';
 
 class TodoCubit extends Cubit<TodoState> {
   final UserState userState;
-  TodoCubit({required this.userState}) : super(TodoInitial());
+  TodoCubit({required this.userState}) : super(TodoInitial()) {
+    Logger().log(Level.info, 'Inside TodoCubit consructor');
+  }
 }
