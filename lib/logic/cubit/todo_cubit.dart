b50d0e1c3dd9de645562e661106332011efc2dd0
Talha <talha.kerpicci@gmail.com>
2021-10-06 18:39:30 +0300

    Finished getting todos

--- lib/logic/cubit/todo_cubit.dart
+++ lib/logic/cubit/todo_cubit.dart
@@ -1,15 +1,31 @@
 import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
+
+import '../../data/services/api.dart';
 import '../../data/model/todo_models.dart';
 import 'user_cubit.dart';
 
 part 'todo_state.dart';
 
 class TodoCubit extends Cubit<TodoState> {
+  final Api api = Api();
   final UserState userState;
+
   TodoCubit({required this.userState}) : super(TodoInitial()) {
     Logger().log(Level.info, 'Inside TodoCubit consructor');
+    getTodos();
+  }
+
+  Future<void> getTodos() async {
+    emit(TodosLoading());
+
+    GetTodosResult getTodosResult = await api.getUserTodos(userId: (userState as UserLoggedIn).loggedInUser!.id!);
+
+    if (getTodosResult.success) {
+      emit(TodosSuccess(todos: getTodosResult.todos));
+    } else {
+      emit(TodosFailed());
+    }
   }
 }
