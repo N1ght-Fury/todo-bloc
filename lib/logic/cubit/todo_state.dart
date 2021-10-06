bf36785459be43af06e79d418a4bc1a5cc805f46
Talha <talha.kerpicci@gmail.com>
2021-10-06 18:39:46 +0300

    Added other todo states

--- lib/logic/cubit/todo_state.dart
+++ lib/logic/cubit/todo_state.dart
@@ -7,4 +7,24 @@ abstract class TodoState {
 
 class TodoInitial extends TodoState {}
 
-class GetTodos extends TodoState {}
+class TodosLoading extends TodoState {}
+
+class TodosFailed extends TodoState {}
+
+class TodosSuccess extends TodoState {
+  final List<Todo?>? todos;
+
+  const TodosSuccess({
+    this.todos,
+  });
+
+  /* factory TodosSuccess.fromMap(Map<String, dynamic> map) {
+    return TodosSuccess(
+        /* todos: User.fromMap(map), */
+        );
+  }
+
+  factory TodosSuccess.fromJson(String source) => TodosSuccess.fromMap(json.decode(source)); */
+
+  /* Map<String, dynamic>? toMap() => todos!.toMap(); */
+}
