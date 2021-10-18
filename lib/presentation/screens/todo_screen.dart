e52a57f2ecc7e2f09106614cd1fc9870ba2f812c
Talha <talha.kerpicci@gmail.com>
2021-10-18 18:00:51 +0300

    Seperated cubits

--- lib/presentation/screens/todo_screen.dart
+++ lib/presentation/screens/todo_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:loading_overlay/loading_overlay.dart';
+import 'package:todo_bloc/logic/cubit/add_todo_cubit.dart';
 
 import '../../data/model/todo_models.dart';
 import '../../logic/cubit/todo_cubit.dart';
@@ -26,60 +27,46 @@ class _HomeScreenState extends State<TodoScreen> {
     );
   }
 
-  /* @override
+  @override
   void initState() {
     super.initState();
     context.read<TodoCubit>().getTodos();
-  } */
+  }
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<TodoCubit, TodoState>(
-      listener: (context, state) {
-        if (state is TodosFailed) {
-          showSnackMessag(context, message: 'Failed to get todos');
-        } else if (state is TodosSuccess) {
-          todos = state.todos;
-        } else if (state is AddTodoSuccess) {
-          showSnackMessag(context, message: 'Successfully created todo!');
-          todos!.add(state.todo);
-        } else if (state is UpdateTodoSuccess) {
-          showSnackMessag(context, message: 'Successfully updated todo!');
-          todos![todos!.indexWhere((todo) => todo!.id == state.todo.id)] = state.todo;
-        } else if (state is DeleteTodoSuccess) {
-          showSnackMessag(context, message: 'Successfully deleted todo!');
-          todos!.removeWhere((todo) => todo!.id == state.id);
-        }
-      },
-      builder: (context, state) {
-        TodoState todoState = context.watch<TodoCubit>().state;
-
-        return Scaffold(
-          drawer: CustomDrawer(context),
-          appBar: AppBar(
-            title: const Text('My Todos'),
-            actions: [
-              IconButton(
-                icon: Icon(Icons.add),
-                onPressed: () => Navigator.pushNamed(context, '/addTodo'),
-              ),
-            ],
+    TodoState state = context.watch<TodoCubit>().state;
+    return Scaffold(
+      drawer: CustomDrawer(context),
+      appBar: AppBar(
+        title: const Text('My Todos'),
+        actions: [
+          BlocProvider<AddTodoCubit>(
+            create: (context) => AddTodoCubit(),
+            child: Builder(
+              builder: (context) {
+                return IconButton(
+                  icon: Icon(Icons.add),
+                  onPressed: () => Navigator.pushNamed(context, '/addTodo'),
+                );
+              },
+            ),
           ),
-          body: LoadingOverlay(
-            isLoading: todoState is TodosLoading,
-            opacity: 0.5,
-            progressIndicator: const CircularProgressIndicator(),
-            child: todoState is! TodosFailed
-                ? ListView.builder(
-                    itemCount: todos!.length,
-                    itemBuilder: (context, index) => TodoCard(
-                      todo: todos![index]!,
-                    ),
-                  )
-                : Container(),
-          ),
-        );
-      },
+        ],
+      ),
+      body: LoadingOverlay(
+        isLoading: state is TodosLoading,
+        opacity: 0.5,
+        progressIndicator: const CircularProgressIndicator(),
+        child: state is! TodosFailed
+            ? ListView.builder(
+                itemCount: (state is TodosSuccess) ? state.todos!.length : 0,
+                itemBuilder: (context, index) => TodoCard(
+                  todo: (state as TodosSuccess).todos![index]!,
+                ),
+              )
+            : Container(),
+      ),
     );
   }
 }
