d4d5138c8d03052b7ba4ef7b4f81fa04e93f3382
Talha <talha.kerpicci@gmail.com>
2021-10-18 18:00:15 +0300

    Seperated cubits

--- lib/logic/cubit/todo_cubit.dart
+++ lib/logic/cubit/todo_cubit.dart
@@ -5,71 +5,31 @@ import 'package:logger/logger.dart';
 import '../../data/model/todo_models.dart';
 import '../../data/services/api.dart';
 
+import '../../locator.dart';
 import 'user_cubit.dart';
 part 'todo_state.dart';
 
 class TodoCubit extends Cubit<TodoState> {
-  final Api api = Api();
-  final UserCubit userCubit;
+  final Api api = getIt<Api>();
 
-  TodoCubit({required this.userCubit}) : super(TodoInitial()) {
+  TodoCubit() : super(TodoInitial()) {
     Logger().log(Level.info, 'Inside TodoCubit consructor');
     // TODO: ASK WHERE I SHOULD CALL THE GETTODOS FUNCTION
     // Also how do i get todos each time after adding a new todo if i dont want to show progress indicator?
     // Lastly should cubit classes be generated with freezed?
     // Api dosyasında fromJson içlerini elle verdik. Nasıl düzeltebiliriz?
-    getTodos();
+    /* getTodos(); */
   }
 
-  /* void onUserLogOut() {
-    emit(TodoInitial());
-  }
- */
   Future<void> getTodos() async {
     emit(TodosLoading());
 
-    GetTodosResult getTodosResult = await api.getUserTodos(userId: (userCubit.state as UserLoggedIn).loggedInUser!.id!);
+    GetTodosResult getTodosResult = await api.getUserTodos(userId: (getIt<UserCubit>().state as UserLoggedIn).loggedInUser!.id!);
 
     if (getTodosResult.success) {
       emit(TodosSuccess(todos: getTodosResult.todos));
     } else {
       emit(TodosFailed());
-    }
-  }
-
-  Future<void> addTodo({required Todo todo}) async {
-    emit(AddTodoLoading());
-
-    CreateTodoResult createTodoResult = await api.createTodo(todo: todo);
-
-    if (createTodoResult.success) {
-      emit(AddTodoSuccess(todo: createTodoResult.todo!));
-    } else {
-      emit(AddTodoFail());
-    }
-  }
-
-  Future<void> updateTodo({required Todo todo}) async {
-    emit(UpdateTodoLoading());
-
-    UpdateTodoResult updateTodoResult = await api.updateTodo(todo: todo);
-
-    if (updateTodoResult.success) {
-      emit(UpdateTodoSuccess(todo: updateTodoResult.todo!));
-    } else {
-      emit(UpdateTodoFail());
-    }
-  }
-
-  Future<void> deleteTodo({required Todo todo}) async {
-    emit(DeleteTodoLoading());
-
-    bool success = await api.deleteTodo(todo: todo);
-
-    if (success) {
-      emit(DeleteTodoSuccess(id: todo.id!));
-    } else {
-      emit(DeleteTodoFail());
     }
   }
 }
