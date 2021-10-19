3eb49a172724227ba44af6314a06a710bab3e142
Talha <talha.kerpicci@gmail.com>
2021-10-19 18:24:53 +0300

    Added MultiBlocListener

--- lib/presentation/screens/add_todo_screen.dart
+++ lib/presentation/screens/add_todo_screen.dart
@@ -1,13 +1,12 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:loading_overlay/loading_overlay.dart';
-import 'package:todo_bloc/logic/cubit/add_todo_cubit.dart';
-import 'package:todo_bloc/logic/cubit/delete_todo_cubit.dart';
-import 'package:todo_bloc/logic/cubit/update_todo_cubit.dart';
+
 import '../../data/model/todo_models.dart';
-import '../../logic/cubit/todo_cubit.dart';
+import '../../logic/cubit/add_todo_cubit.dart';
+import '../../logic/cubit/delete_todo_cubit.dart';
+import '../../logic/cubit/update_todo_cubit.dart';
 import '../../logic/cubit/user_cubit.dart';
 
 class AddTodoScreen extends StatefulWidget {
@@ -84,42 +83,6 @@ class _AddTodoScreenState extends State<AddTodoScreen> {
     } else {
       todo = Todo(userId: (context.read<UserCubit>().state as UserLoggedIn).loggedInUser!.id, title: '', completed: false);
     }
-
-    addCubitStream = context.read<AddTodoCubit>().stream.listen((state) {
-      if (state is AddTodoLoading) {
-        setState(() {
-          isLoading = true;
-        });
-      } else if (state is AddTodoFail) {
-        showSnackMessag(context, message: 'Failed to add todo');
-      } else if (state is AddTodoSuccess) {
-        Navigator.pop(context);
-      }
-    });
-
-    updateCubitStream = context.read<UpdateTodoCubit>().stream.listen((state) {
-      if (state is UpdateTodoFail) {
-        showSnackMessag(context, message: 'Failed to update todo');
-      } else if (state is UpdateTodoSuccess) {
-        Navigator.pop(context);
-      }
-    });
-
-    deleteCubitStream = context.read<DeleteTodoCubit>().stream.listen((state) {
-      if (state is UpdateTodoFail) {
-        showSnackMessag(context, message: 'Failed to update todo');
-      } else if (state is UpdateTodoSuccess) {
-        Navigator.pop(context);
-      }
-    });
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    addCubitStream!.cancel();
-    updateCubitStream!.cancel();
-    deleteCubitStream!.cancel();
   }
 
   @override
@@ -142,71 +105,103 @@ class _AddTodoScreenState extends State<AddTodoScreen> {
               : Container(),
         ],
       ),
-      body: BlocConsumer<TodoCubit, TodoState>(
-        listener: (context, state) {
-          if (state is DeleteTodoFail) {
-            showSnackMessag(context, message: 'Failed to delete todo');
-          } else if (state is DeleteTodoSuccess) {
-            Navigator.pop(context);
-          }
-        },
-        builder: (context, state) {
-          return LoadingOverlay(
-            isLoading: isLoading,
-            opacity: 0.5,
-            progressIndicator: const CircularProgressIndicator(),
-            child: Padding(
-              padding: EdgeInsets.only(top: 15.0, right: 10.0, left: 10.0),
-              child: ListView(
-                children: <Widget>[
-                  SizedBox(height: 15),
-                  TextField(
-                    controller: titleController,
-                    onChanged: (String value) {
-                      todo = todo!.copyWith(title: value);
-                    },
-                    decoration: InputDecoration(
-                      labelText: 'Title',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(5.0),
+      body: MultiBlocListener(
+        listeners: [
+          BlocListener<AddTodoCubit, AddTodoState>(
+            listener: (context, state) {
+              if (state is AddTodoLoading) {
+                setState(() {
+                  isLoading = true;
+                });
+              } else if (state is AddTodoFail) {
+                showSnackMessag(context, message: 'Failed to add todo');
+              } else if (state is AddTodoSuccess) {
+                Navigator.pop(context);
+              }
+            },
+          ),
+          BlocListener<UpdateTodoCubit, UpdateTodoState>(
+            listener: (context, state) {
+              if (state is UpdateTodoLoading) {
+                setState(() {
+                  isLoading = true;
+                });
+              } else if (state is UpdateTodoFail) {
+                showSnackMessag(context, message: 'Failed to update todo');
+              } else if (state is UpdateTodoSuccess) {
+                Navigator.pop(context);
+              }
+            },
+          ),
+          BlocListener<DeleteTodoCubit, DeleteTodoState>(
+            listener: (context, state) {
+              if (state is DeleteTodoLoading) {
+                setState(() {
+                  isLoading = true;
+                });
+              } else if (state is DeleteTodoFail) {
+                showSnackMessag(context, message: 'Failed to delete todo');
+              } else if (state is DeleteTodoSuccess) {
+                Navigator.pop(context);
+              }
+            },
+          ),
+        ],
+        child: LoadingOverlay(
+          isLoading: isLoading,
+          opacity: 0.5,
+          progressIndicator: const CircularProgressIndicator(),
+          child: Padding(
+            padding: EdgeInsets.only(top: 15.0, right: 10.0, left: 10.0),
+            child: ListView(
+              children: <Widget>[
+                SizedBox(height: 15),
+                TextField(
+                  controller: titleController,
+                  onChanged: (String value) {
+                    todo = todo!.copyWith(title: value);
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Title',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5.0),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 15),
+                Container(
+                  padding: EdgeInsets.only(left: 5),
+                  child: Row(
+                    children: [
+                      Text('Is Completed: '),
+                      SizedBox(
+                        width: 10,
                       ),
-                    ),
+                      Switch(
+                        value: todo!.completed,
+                        onChanged: (bool value) {
+                          setState(() {
+                            todo = todo!.copyWith(completed: value);
+                          });
+                        },
+                      ),
+                    ],
                   ),
-                  SizedBox(height: 15),
-                  Container(
-                    padding: EdgeInsets.only(left: 5),
-                    child: Row(
-                      children: [
-                        Text('Is Completed: '),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Switch(
-                          value: todo!.completed,
-                          onChanged: (bool value) {
-                            setState(() {
-                              todo = todo!.copyWith(completed: value);
-                            });
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      if (widget.todo != null) {
-                        context.read<UpdateTodoCubit>().updateTodo(todo: todo!);
-                      } else {
-                        context.read<AddTodoCubit>().addTodo(todo: todo!);
-                      }
-                    },
-                    child: Text(widget.todo != null ? 'Update' : 'Add'),
-                  ),
-                ],
-              ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (widget.todo != null) {
+                      context.read<UpdateTodoCubit>().updateTodo(todo: todo!);
+                    } else {
+                      context.read<AddTodoCubit>().addTodo(todo: todo!);
+                    }
+                  },
+                  child: Text(widget.todo != null ? 'Update' : 'Add'),
+                ),
+              ],
             ),
-          );
-        },
+          ),
+        ),
       ),
     );
   }
