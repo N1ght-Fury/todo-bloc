6c14ff10be3a39a8a5cce1017aba29b0936e7d25
Talha <talha.kerpicci@gmail.com>
2021-10-19 18:23:49 +0300

    Import fix

--- lib/data/model/todo_models.dart
+++ lib/data/model/todo_models.dart
@@ -1,5 +1,5 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'todo_models.freezed.dart';
 part 'todo_models.g.dart';
