8e33e094fc3d64eb1fb6b71c065e06cb55288253
Talha <talha.kerpicci@gmail.com>
2021-10-19 18:22:01 +0300

    Registered TodoCubit

--- lib/locator.dart
+++ lib/locator.dart
@@ -2,10 +2,12 @@ import 'package:get_it/get_it.dart';
 
 import 'data/services/api.dart';
 import 'logic/cubit/user_cubit.dart';
+import 'logic/cubit/todo_cubit.dart';
 
 final getIt = GetIt.instance;
 
 void setup() {
   getIt.registerLazySingleton(() => Api());
   getIt.registerFactory<UserCubit>(() => UserCubit());
+  getIt.registerFactory<TodoCubit>(() => TodoCubit());
 }
