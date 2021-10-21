dcb25dfc2b5374d0ce52dccf17a92034cf7427ce
Talha <talha.kerpicci@gmail.com>
2021-10-21 16:10:58 +0300

    Removed comments

--- lib/logic/cubit/todo_cubit.dart
+++ lib/logic/cubit/todo_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:injectable/injectable.dart';
 import 'package:logger/logger.dart';
 
 import '../../data/model/todo_models.dart';
@@ -7,24 +8,21 @@ import '../../data/services/api.dart';
 
 import '../../locator.dart';
 import 'user_cubit.dart';
+
 part 'todo_state.dart';
 
+@singleton
 class TodoCubit extends Cubit<TodoState> {
-  final Api api = getIt<Api>();
+  final Api api;
 
-  TodoCubit() : super(TodoInitial()) {
+  TodoCubit(this.api) : super(TodoInitial()) {
     Logger().log(Level.info, 'Inside TodoCubit consructor');
-    // TODO: ASK WHERE I SHOULD CALL THE GETTODOS FUNCTION
-    // Also how do i get todos each time after adding a new todo if i dont want to show progress indicator?
-    // Lastly should cubit classes be generated with freezed?
-    // Api dosyasında fromJson içlerini elle verdik. Nasıl düzeltebiliriz?
-    /* getTodos(); */
   }
 
   Future<void> getTodos() async {
     emit(TodosLoading());
 
-    GetTodosResult getTodosResult = await api.getUserTodos(userId: (getIt<UserCubit>().state as UserLoggedIn).loggedInUser!.id!);
+    GetTodosResult getTodosResult = await api.getUserTodos(userId: (getIt<UserCubit>().state as UserLoggedIn).loggedInUser.id!);
 
     if (getTodosResult.success) {
       emit(TodosSuccess(todos: getTodosResult.todos));
