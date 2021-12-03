267f5b3d425eb2f43e9aefbd89c89161e1b2e651
Talha <talha.kerpicci@gmail.com>
2021-12-03 20:57:25 +0300

    Removed uneccessary column

--- lib/presentation/widgets/todo_card.dart
+++ lib/presentation/widgets/todo_card.dart
@@ -19,91 +19,84 @@ class TodoCard extends StatelessWidget {
           getIt<TodoCubit>().getTodos();
         });
       },
-      child: Column(
-        children: <Widget>[
-          SizedBox(
-            height: 5,
+      child: Container(
+        padding: const EdgeInsets.only(left: 6, right: 6, top: 5),
+        height: 110.0,
+        child: Card(
+          clipBehavior: Clip.antiAlias,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
           ),
-          Container(
-            padding: const EdgeInsets.only(left: 6, right: 6),
-            height: 110.0,
-            child: Card(
-              clipBehavior: Clip.antiAlias,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
+          color: Colors.white,
+          child: Row(
+            children: <Widget>[
+              Container(
+                color: todo.completed ? Colors.green : Colors.red,
+                width: 5,
               ),
-              color: Colors.white,
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    color: todo.completed ? Colors.green : Colors.red,
-                    width: 5,
-                  ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Expanded(
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        Align(
-                          alignment: Alignment.centerLeft,
-                          child: Container(
-                            padding: const EdgeInsets.only(left: 10, bottom: 5),
-                            child: Text(
-                              'Id: ${todo.id}',
-                              style: const TextStyle(
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
-                                color: Color(0xff717D8D),
-                              ),
-                            ),
+              SizedBox(
+                width: 10.0,
+              ),
+              Expanded(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Container(
+                        padding: const EdgeInsets.only(left: 10, bottom: 5),
+                        child: Text(
+                          'Id: ${todo.id}',
+                          style: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                            color: Color(0xff717D8D),
                           ),
                         ),
-                        SizedBox(
-                          height: 5.0,
-                        ),
-                        Align(
-                          alignment: Alignment.centerLeft,
-                          child: Container(
-                            padding: const EdgeInsets.only(
-                              left: 10,
-                            ),
-                            child: Text(
-                              'Title: ${todo.title}',
-                              style: const TextStyle(
-                                fontSize: 14,
-                                color: Color(0xff707070),
-                              ),
-                            ),
-                          ),
-                        ),
-                        SizedBox(
-                          height: 5.0,
-                        ),
-                        Align(
-                          alignment: Alignment.centerLeft,
-                          child: Container(
-                            padding: const EdgeInsets.only(
-                              left: 10,
-                            ),
-                            child: Text(
-                              'State: ${todo.completed ? 'Completed' : 'Pending'}',
-                              style: const TextStyle(
-                                fontSize: 14,
-                                color: Color(0xff707070),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ],
+                      ),
                     ),
-                  ),
-                ],
+                    SizedBox(
+                      height: 5.0,
+                    ),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Container(
+                        padding: const EdgeInsets.only(
+                          left: 10,
+                        ),
+                        child: Text(
+                          'Title: ${todo.title}',
+                          style: const TextStyle(
+                            fontSize: 14,
+                            color: Color(0xff707070),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5.0,
+                    ),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Container(
+                        padding: const EdgeInsets.only(
+                          left: 10,
+                        ),
+                        child: Text(
+                          'State: ${todo.completed ? 'Completed' : 'Pending'}',
+                          style: const TextStyle(
+                            fontSize: 14,
+                            color: Color(0xff707070),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
